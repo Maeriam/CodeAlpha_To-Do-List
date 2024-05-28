db246081a09547ea5cbda88b769bfdd9140c6a11
Mariam Adedeji <Mariam.adedejimariam600@gmail.com>
2024-05-28 18:34:04 +0100

    updated

---
+++ script.js
@@ -0,0 +1,50 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+const addButton = document.getElementById("add-button");
+const clearButton = document.getElementById("clear-button");
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write something");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"; // Unicode character for multiplication sign
+        span.classList.add("delete-button"); // Adding a class for easy styling
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+addButton.addEventListener("click", addTask);
+
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    let savedData = localStorage.getItem("data");
+    if (savedData) {
+        listContainer.innerHTML = savedData;
+    }
+}
+
+clearButton.addEventListener("click", function () {
+    localStorage.removeItem("data");
+    listContainer.innerHTML = "";
+});
+
+showTask();
